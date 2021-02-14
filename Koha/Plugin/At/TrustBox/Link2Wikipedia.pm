219f2636d6947a77daf285ee596617a3c4a7d026
Mark Hofstetter <mark@hofstetter.at>
2021-02-14 15:38:57 +0100

    initial commit - working

---
+++ Koha/Plugin/At/TrustBox/Link2Wikipedia.pm
@@ -0,0 +1,104 @@
+package Koha::Plugin::At::TrustBox::Link2Wikipedia;
+
+use Modern::Perl;
+
+use base qw(Koha::Plugins::Base);
+use C4::Context;
+use Cwd qw(abs_path);
+
+use Koha::Authorities;
+use C4::AuthoritiesMarc;
+
+use Mojo::JSON qw(decode_json);;
+
+our $VERSION = "0.2";
+
+our $metadata = {
+    name            => 'Link2Wikipedia Plugin',
+    author          => 'Mark Hofstetter',
+    date_authored   => '2021-02-05',
+    date_updated    => "2021-02-05",
+    minimum_version => '19.05.00.000',
+    maximum_version => undef,
+    version         => $VERSION,
+    description     => 'this plugin generates extracts & links to wikipedia articles via GND lookup http://tools.wmflabs.org/persondata/redirect/gnd/de/',
+};
+
+sub new {
+    my ( $class, $args ) = @_;
+
+    $args->{'metadata'} = $metadata;
+    $args->{'metadata'}->{'class'} = $class;
+
+    my $self = $class->SUPER::new($args);
+    $self->{cgi} = CGI->new();
+
+    return $self;
+}
+
+sub api_routes {
+    my ( $self, $args ) = @_;
+    my $spec_str = $self->mbf_read('openapi.json');
+    my $spec = decode_json($spec_str);
+    return $spec;
+}
+
+sub api_namespace {
+    my ( $self ) = @_;
+    return 'link2wikipedia';
+}
+
+sub opac_head {
+    my ( $self ) = @_;
+}
+
+sub opac_js {
+    my ( $self ) = @_;
+    my $cgi = $self->{'cgi'};
+    return unless $cgi->script_name =~ /opac-authoritiesdetail.pl/;
+    my $authid = $cgi->param('authid');
+    return unless $authid;
+    my $record = GetAuthority( $authid );
+
+    my $authority = Koha::Authorities->find( $authid );
+    my @f35 =  $record->field('035')->subfields;
+    my $gnd = $f35[0][1];
+    $gnd =~ s/\(.*\)//;
+
+    my $js = "<script> var gnd = '$gnd' \n";
+    $js .= <<'JS';
+    var page = $('body').attr('ID');
+    var lang = $('html').attr('lang');
+    if (page == "opac-authoritiesdetail") {
+
+        $(function(e) {
+            var ajaxData = { 'id': gnd };
+            $.ajax({
+              url: '/api/v1/contrib/link2wikipedia/gnd',
+            type: 'GET',
+            dataType: 'json',
+            data: ajaxData,
+        })
+        .done(function(data) {
+            console.log('fetched data from wikipedia');
+            $('<div class="newscontainer"><div class="media"><span class="label">Wikipedia</span><br>' + 
+            '<span class="float-left media-left mx-2"><img id="wikipedia_image" src=""/></span>' + 
+            '<div class="media-body"><div id="wikipedia_extract"></div><a id="wikipedia_url" target="_blank" href="#">' + 
+            '<img src="https://upload.wikimedia.org/wikipedia/commons/6/62/Banner_80x15.png"></a></div></div></div>')
+            .insertAfter( ".usedin" );
+            $('#wikipedia_extract').html(data.content);
+            $("#wikipedia_url").attr("href", data.wikipedia_url);
+            $("#wikipedia_image").attr("src", data.image_url).load(function(){
+                 this.width;
+                });
+            
+            })
+        .error(function(data) {});
+        });
+
+    }
+    </script>
+JS
+    
+    return $js;
+}
---
+++ Koha/Plugin/At/TrustBox/Link2Wikipedia/Link2WikipediaController.pm
@@ -0,0 +1,79 @@
+package Koha::Plugin::At::TrustBox::Link2Wikipedia::Link2WikipediaController;
+
+use Mojo::Base 'Mojolicious::Controller';
+
+use C4::Context;
+use C4::Debug;
+use C4::Output qw(:html :ajax pagination_bar);
+
+use HTTP::Request;
+use LWP::UserAgent;
+
+# use JSON;
+
+my $header = ['Content-Type' => 'application/json; charset=UTF-8'];
+
+use Mojo::JSON qw(decode_json encode_json);
+
+my $translate = {
+    'de-DE' => 
+        {dt      => 'https://cdn.datatables.net/plug-ins/1.10.21/i18n/German.json',
+         columns => ['Daten', 'Band', 'Jahr', 'Cover'],
+         label   => 'Bände',
+        },
+    'si-SI' => 
+        {dt      => 'https://cdn.datatables.net/plug-ins/1.10.21/i18n/Slovenian.json',
+        }
+};
+
+sub gnd_redirect {
+    my $gnd = shift;
+    my $url='https://persondata.toolforge.org/redirect/gnd/de/';
+    my $request = HTTP::Request->new(GET => $url.$gnd);
+    my $ua = LWP::UserAgent->new;
+    my $response = $ua->request($request);
+    my @redirects = $response->redirects;
+    my $location = $redirects[-1]->headers->{location};
+    $location =~ /.*\/(.*$)/;    
+    return ($location, $1);
+}
+
+sub get_extract {
+    my $title = shift;
+    my $article_url = 'https://de.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles='.$title;
+    my $r = HTTP::Request->new('GET', $article_url, $header);
+    my $ua = LWP::UserAgent->new();
+    my $res = $ua->request($r);
+    my $content = decode_json($res->content);
+    my @keys = keys %{$content->{query}->{pages}};
+    return $content->{query}->{pages}->{$keys[0]}->{extract}
+}
+
+sub get_pageimage {
+    my $title = shift;
+    my $pageimage_url = 'https://de.wikipedia.org/w/api.php?format=json&action=query&prop=pageimages&redirects=1&pithumbsize=200&titles='.$title;
+    my $r = HTTP::Request->new('GET', $pageimage_url, $header);
+    my $ua = LWP::UserAgent->new();
+    my $res = $ua->request($r);
+    my $content = decode_json($res->content);
+    my @keys = keys %{$content->{query}->{pages}};
+    return $content->{query}->{pages}->{$keys[0]}->{thumbnail}->{source};
+}
+
+sub get {
+    my $c = shift->openapi->valid_input or return;
+    my $gnd_id = $c->validation->param('id');
+    my ($wikipedia_url, $wikipedia_title) = gnd_redirect($gnd_id);
+    my $content = get_extract($wikipedia_title);
+    my $image_url = get_pageimage($wikipedia_title);
+    
+    return $c->render( status => 200, openapi => {
+        content => $content,
+        wikipedia_url => $wikipedia_url,
+        image_url => $image_url,
+    } );
+
+}      
+
+1;
+
